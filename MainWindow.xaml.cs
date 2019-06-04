be2eb819b83c2dae23259a225e1dd6ca461134ed
davidlaughton11 <47399940+davidlaughton11@users.noreply.github.com>
2019-06-04 14:42:05 -0400

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CulminatingProblemJ1TournamentSelection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int score;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (OneInput.Text == "W")
+            {
+                score++;
+            }
+            if (TwoInput.Text == "W")
+            {
+                score++;
+            }
+            if (ThreeInput.Text == "W")
+            {
+                score++;
+            }
+            if (FourInput.Text == "W")
+            {
+                score++;
+            }
+            if (FiveInput.Text == "W")
+            {
+                score++;
+            }
+            if (SixInput.Text == "W")
+            {
+                score++;
+            }
+            if (score >= 5)
+            {
+                lblOutput.Content = "1";
+            }
+            if (score == 3 || score == 4)
+            {
+                lblOutput.Content = "2";
+            }
+            if (score == 2 || score == 1)
+            {
+                lblOutput.Content = "3";
+            }
+            if (score < 1)
+            {
+                lblOutput.Content = "-1";
+            }
+        }
+    }
+}
